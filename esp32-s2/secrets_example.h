f1ef3069c136c636fc339026e70220162d06c3e9
Jean-François Lamy <jf@jflamy.dev>
2022-02-12 12:57:19 -0500

    renamed to esp32

---
+++ esp32-s2/secrets_example.h
@@ -0,0 +1,12 @@
+// wifi
+// Wokwi-GUEST is used by the ESP32 simulator.
+// Change when using a real device.
+const char* wifiSSID = "Wokwi-GUEST";
+const char* wifiPassword = "";
+
+// MQTT server
+// On the cloud, make sure you use a server that enforces username and password
+// Create a Heroku free application and add StackHero Mosquitto as an add-on (test mode is free).
+const char* mqttServer = "test.mosquitto.org";
+const char* mqttUserName= "";
+const char* mqttPassword = "";
